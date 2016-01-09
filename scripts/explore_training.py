7da38f3cb50e1478b32fc9304273018de43c4ee8
Joel Piper <joelmpiper@gmail.com>
2016-01-08 21:26:58 -0500

    Added ability to cut on training output

--- scripts/explore_training.py
+++ scripts/explore_training.py
@@ -23,7 +23,8 @@ import cPickle as pickle
     tidy the training set by moving the location code from a column to
     to a row
 '''
-def create_training_data(train_dir):
+def create_training_data(train_dir, station=[], lat=[], lon=[],
+                         elev=[]):
     train_data = pd.read_csv(train_dir + '/train.csv',
                              index_col='Date', parse_dates=['Date'])
     rot_df = pd.DataFrame(train_data.unstack().reset_index()[:])
@@ -36,7 +37,50 @@ def create_training_data(train_dir):
     loc_df['elon'] = loc_df['elon'] + 360
     loc_df.rename(columns={'nlat':'lat','elon':'lon'}, inplace=True)
     loc_df.index.names = ['station']
+
+    # if we are making any specifications on the output data, then
+    # reduce what is returned to conform with those specifications
+    if(station or lat or lon or elev):
+        rot_df, loc_df = reduced_training(rot_df, loc_df, station, lat,
+                                         lon, elev)
     return rot_df, loc_df
+
+
+''' Take the output data and location data and return the reduced set
+'''
+def reduced_training(rot_df, loc_df, station, lat, lon, elev):
+    if (station):
+        mod_train = rot_df[rot_df['location'].isin(station)]
+        mod_loc = loc_df.loc[station,:]
+    else:
+        mod_train = rot_df
+        mod_loc = loc_df
+        if (lat):
+            mod_train, mod_loc = cut_var(mod_train, mod_loc, lat, 'lat',
+                    rot_df.columns, loc_df.columns)
+        if (lon):
+            mod_train, mod_loc = cut_var(mod_train, mod_loc, lon, 'lon',
+                    rot_df.columns, loc_df.columns)
+        if (elev):
+            mod_train, mod_loc = cut_var(mod_train, mod_loc, elev, 'elev',
+                    rot_df.columns, loc_df.columns)
+    return mod_train, mod_loc
+
+
+''' Reduce on a single variable '''
+def cut_var(train, loc, var, var_name, train_split, loc_split):
+    combined = train.merge(loc, left_on='location',
+                                right_index=True)
+
+    combined = combined[(combined[var_name] >= var[0]) &
+                        (combined[var_name] <= var[1])]
+    mod_train = combined[train_split].drop_duplicates()
+    mod_loc = combined[loc_split.union(['location'])]
+    mod_loc = mod_loc.reset_index().drop('date', axis=1)
+    mod_loc.rename(columns={'location':'station'}, inplace=True)
+    mod_loc = mod_loc.drop_duplicates().set_index('station')
+
+    return mod_train, mod_loc
 
 
 ''' Create the indices that will be used for all of the variables.
@@ -144,5 +188,9 @@ def input_subset(file_list, dates, models, times, lats, lons):
 '''
 def extract_subset(input_dir, output_dir, **kwargs):
 
-    rot_df, loc_df = create_training_data(input_dir)
+    train_params = {}
+    for key, value in kwargs.iteritems():
+        if key in ['station', 'lat', 'lon', 'elev']:
+            train_params[key] = value
+    rot_df, loc_df = create_training_data(input_dir, **kwargs)
     return rot_df, loc_df
